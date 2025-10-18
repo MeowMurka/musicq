55a9e9dff58c1f577560a170eb0d4448134e07e7
MeowMurka <danya.rayushkin@mail.ru>
2025-10-18 11:50:07 +0500

    wire frontend to Render API

--- frontend/static/config.js
+++ frontend/static/config.js
@@ -1,7 +1,4 @@
-<!-- frontend/static/config.js -->
 <script>
-  // после создания API на Render сюда вставишь его URL, например:
-  // window.API_BASE = "https://musicq-api.onrender.com";
-  window.API_BASE = "";
+  window.API_BASE = "https://musicq.onrender.com";  // <-- поставь свой точный URL API
 </script>
 
